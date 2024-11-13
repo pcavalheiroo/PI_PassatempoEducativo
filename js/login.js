265e2adf96053ee6399b351d9875124ce2ea0fb7
Dantefl1 <dantefernandessilvalima@gmail.com>
2024-11-13 08:41:18 -0300

    Funcionando

---
+++ js/login.js
@@ -0,0 +1,41 @@
+document.getElementById('loginButton').addEventListener('click', async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    function exibirAlerta(seletor, innerHTML, classesToAdd, classesToRemove, timeout) {
+        let alert = document.querySelector(seletor);
+        alert.innerHTML = innerHTML;
+        alert.classList.add(...classesToAdd);
+        alert.classList.remove(...classesToRemove);
+        setTimeout(() => {
+            alert.classList.remove(...classesToAdd);
+            alert.classList.add(...classesToRemove);
+        }, timeout);
+    }
+    
+    if (!email || !password) {
+        exibirAlerta('.alert', 'Preencha todos os campos!', ['show'], ['d-none'], 2000);
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ login: email, password: password })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            exibirAlerta('.alert', 'Login realizado com sucesso!', ['show'], ['d-none'], 2000);
+        } else {
+            exibirAlerta('.alert', 'Não foi possível realizar o login.', ['show'], ['d-none'], 2000);
+        }
+    } catch (e) {
+        console.error(e);
+        exibirAlerta('.alert', 'Erro ao tentar se conectar. Tente novamente mais tarde.', ['show'], ['d-none'], 2000);
+    }
+});
